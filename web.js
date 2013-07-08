8476877c26c3127af834529c7c73caf2105df143
Sourabh Tripathi <sourabhtripathi@outlook.com>
2013-07-08 06:11:45 +0000

    1.2 index

--- web.js
+++ web.js
@@ -2,4 +2,16 @@
 var fs = require('fs');
 var file = "index.html";
 var buffer = new Buffer(fs.readFileSync(file));
-console.log(buffer.toString());
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+    response.send(buffer.toString());
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+    console.log("Listening on " + port);
+});
+//console.log(buffer.toString());
